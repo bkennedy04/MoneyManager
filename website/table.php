751d42c5c43d1bcbdb32b83a41976fa18f43099a
bkennedy04 <bkennedy04@bellarmine.edu>
2017-03-27 16:17:25 -0400

    Added "table.php" to generate transaction table

---
+++ website/table.php
@@ -0,0 +1,46 @@
+<?php
+
+$servername = "localhost";
+$username = "brooke";
+$password = "root";
+$dbname = "Money_Manager";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$query1 = "SELECT transaction_amount, category, date, sign FROM transactions";
+$result1 = $conn->query($query1);
+$var1;
+$var2;
+
+
+	if ($result1->num_rows > 0) {
+    // output data of each row
+		while($row = $result1->fetch_assoc()) {
+		
+			if($row["sign"] == "-"){
+				$var1 = "danger";
+				$var2 = "glyphicon glyphicon-arrow-down";
+			}
+			else{
+				$var1 = "success";
+				$var2 = "glyphicon glyphicon-arrow-up";
+			}
+?>
+
+	        <tr class="<?php echo $var1; ?>">
+              <td><span class="<?php echo $var2;?>" aria-hidden="true"><?php echo $row["transaction_amount"]; ?> </span></td>
+              <td><?php echo $row["category"];?></td>
+              <td><?php echo $row["date"];?></td>
+            </tr>
+
+
+<?php
+		}
+	}
+	else{
+		echo "0 results";
+	}
+?>
