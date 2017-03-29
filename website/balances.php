0f45fa6a1222108a51d223fa6d747e3f059d58ca
dkelly02 <dkelly02@bellarmine.edu>
2017-03-29 15:56:59 -0400

    add

--- website/balances.php
+++ website/balances.php
@@ -1,12 +1,18 @@
 <?php
-include	"moneymanager.php";
+include	"connect_db.php";
 
-$query1="SELECT Date from transactions";
-$query2="SELECT AccountBalance from transactions";
+$query1="SELECT date from transactions";
+$query2="SELECT account_balance from transactions order by date";
 $result1=$conn->query($query1);
 $result2=$conn->query($query2);
 
-	if($result2->num_rows>0){
-		while($row=$result2->fetch.array()){
-		echo $row["AccountBalance".","];
-	}	} ?>
+echo "[";
+if($result2->num_rows>0){
+		while($row=$result2->fetch_assoc()){
+			echo $row["account_balance"].",";
+	}	}
+	else{
+	echo "0 results";}
+
+echo "]"; 
+?>
--- website/dates.php
+++ website/dates.php
@@ -1,12 +1,17 @@
 <?php
-include	"moneymanager.php";
+include	"connect_db.php";
 
-$query1="SELECT Date from transactions";
-$query2="SELECT AccountBalance from transactions";
+$query1="SELECT date from transactions order by date";
+$query2="SELECT account_balance from transactions";
 $result1=$conn->query($query1);
 $result2=$conn->query($query2);
 
+echo "[";
 if($result1->num_rows>0){
 	while($row=$result1->fetch_array()){
-	echo $row["'"."Date"."'".","];
-		}	}	?>
+		echo "\"" .$row["date"] ."\",";
+		}	}
+	else{
+	echo "0 results";}
+echo"]";
+	?>
