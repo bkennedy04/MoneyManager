3d82b78448f5346c73ac281a3550e0de5e47920b
dkelly02 <dkelly02@bellarmine.edu>
2017-03-29 15:16:16 -0400

    Summary

---
+++ website/balances.php
@@ -0,0 +1,12 @@
+<?php
+include	"moneymanager.php";
+
+$query1="SELECT Date from transactions";
+$query2="SELECT AccountBalance from transactions";
+$result1=$conn->query($query1);
+$result2=$conn->query($query2);
+
+	if($result2->num_rows>0){
+		while($row=$result2->fetch.array()){
+		echo $row["AccountBalance".","];
+	}	} ?>
