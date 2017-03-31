626441a66e7b0ad6f0179164e0d6a624600b2fef
bkennedy04 <bkennedy04@bellarmine.edu>
2017-03-30 23:20:20 -0400

    Added goals functionality prototype

---
+++ website/goals.php
@@ -0,0 +1,38 @@
+
+	
+<?php 
+include "connect_db.php";
+$query0 = "SELECT account_balance FROM transactions WHERE time_added = (SELECT MAX(time_added) FROM transactions)";
+$query1 = "SELECT * FROM goals";
+$result0 = $conn->query($query0);
+$result1 = $conn->query($query1);
+
+if ($result0->num_rows > 0) {
+	if($row = $result0->fetch_array()) {
+		$current_balance = $row["account_balance"];
+	}
+}
+if ($result1->num_rows > 0) {
+	$count = 1;
+
+	// output data of each row
+	while($row = $result1->fetch_array()) {
+	$percentage = $current_balance/ $row["amount"];
+?>
+
+
+        Goal <?php echo $count .": ". $row["amount"] ." by ". $row["end_date"] ." for ". $row["description"]; ?>
+        <div class="progress">
+          <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="min-width: 2em;">
+            <?php echo $percentage ?>%
+          </div>
+        </div>
+<?php
+	}
+}
+else{
+	echo "0 results";
+}
+
+?>
+
--- website/index.php
+++ website/index.php
@@ -32,19 +32,7 @@
     <div class="panel panel-default">
       <div class="panel-heading">Goals</div>
       <div class="panel-body">
-        Goal 1:
-        <div class="progress">
-          <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="min-width: 2em;">
-            0%
-          </div>
-        </div>
-
-        Goal 2:
-        <div class="progress">
-          <div class="progress-bar" role="progressbar" aria-valuenow="2" aria-valuemin="0" aria-valuemax="100" style="min-width: 2em; width: 45%;">
-            45%
-          </div>
-        </div>
+        <?php include "goals.php"; ?>
       </div>
     </div>
 	
