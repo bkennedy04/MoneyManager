9a58de7ba7aa59dfc76887a4fdfbbca09f7e11f2
bkennedy04 <bkennedy04@bellarmine.edu>
2017-04-03 11:45:09 -0400

    Updated forms

--- website/goals_form.php
+++ website/goals_form.php
@@ -6,9 +6,9 @@
       <div class="panel-body">
 
 		<form action="goals_form_processing.php" method="post">
-			Amount:<br><input type="number" step="0.01" name="amount"><br>
-			Description:<br><input type="text" name="description"><br>
-			End date:<br><input type="date" name="end_date"><br>
+			Amount:<br><input type="number" step="0.01" name="amount" required><br>
+			Description:<br><input type="text" name="description" required><br>
+			End date:<br><input type="date" name="end_date" required><br>
 			<br><input type="submit" value="Submit">
 			<input type="reset">
 		</form>
--- website/transaction_form.php
+++ website/transaction_form.php
@@ -6,10 +6,20 @@
       <div class="panel-body">
 
 		<form action="transaction_form_processing.php" method="post">
-			Transaction Amount:<br><input type="number" step="0.01" name="amount"><br>
+			Transaction Amount:<br><input type="number" step="0.01" name="amount" required><br>
 			Description:<br><input type="text" name="description"><br>
-			Category:<br><input type="text" name="category"><br>
-			Transaction Date:<br><input type="date" name="date"><br>
+			Category:<br>  <input list="browsers" name="category" required>
+			  <datalist id="browsers">
+				<option value="Housing">
+				<option value="Utilities">
+				<option value="Health Care">
+				<option value="Food and Groceries">
+				<option value="Personal Care">
+				<option value="Entertainment">
+				<option value="Emergency Fund">
+				<option value="Savings">
+			  </datalist> <br>
+			Transaction Date:<br><input type="date" name="date" required><br>
 			Account Balance:<br><input type="number" step="0.01" name="balance"><br>
 			<br><input type="submit" value="Submit">
 			<input type="reset">
