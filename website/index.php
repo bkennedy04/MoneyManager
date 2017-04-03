d67ebd5cc2472c52b5e7f37a41597dbf7e00d9e8
bkennedy04 <bkennedy04@bellarmine.edu>
2017-04-03 15:37:09 -0400

    Updated bar chart

--- website/index.php
+++ website/index.php
@@ -108,28 +108,32 @@ var ctx = document.getElementById("bar_chart");
 new Chart(ctx, {
     type: 'bar',
     data: {
-		labels: ["January", "February", "March", "April", "May", "June", "July"],
+		labels: ["January", "February"],
 		datasets: [
 			{
-				label: "My First dataset",
+				label: "Expenses",
 				backgroundColor: [
 					'rgba(255, 99, 132, 0.2)',
-					'rgba(54, 162, 235, 0.2)',
-					'rgba(255, 206, 86, 0.2)',
-					'rgba(75, 192, 192, 0.2)',
-					'rgba(153, 102, 255, 0.2)',
-					'rgba(255, 159, 64, 0.2)'
+					'rgba(255, 99, 132, 0.2)'
+
 				],
 				borderColor: [
-					'rgba(255,99,132,1)',
-					'rgba(54, 162, 235, 1)',
-					'rgba(255, 206, 86, 1)',
-					'rgba(75, 192, 192, 1)',
-					'rgba(153, 102, 255, 1)',
-					'rgba(255, 159, 64, 1)'
+					'rgba(54, 162, 235, 0.2)'
 				],
 				borderWidth: 1,
-				data: [65, 59, 80, 81, 56, 55, 40],
+				data: [65, 59],
+			},
+			{
+				label: "Profits",
+				backgroundColor: [
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(75, 192, 192, 0.2)'
+				],
+				borderColor: [
+					'rgba(255, 99, 132, 0.2)'
+				],
+				borderWidth: 1,
+				data: [100, 30],
 			}
 		]
 	},
