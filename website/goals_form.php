c794dc125d362dd7fd15d2e033e20af202701153
bkennedy04 <bkennedy04@bellarmine.edu>
2017-04-02 22:10:22 -0400

    Updated forms

--- website/goals_form.php
+++ website/goals_form.php
@@ -1,26 +1,16 @@
 <?php include "header.php";?>
 <html>
-<div class="col-xs-6" style="padding-left: 0">
+<div class="col-xl-12" style="padding-left: 0">
     <div class="panel panel-default">
       <div class="panel-heading">Enter Goal</div>
       <div class="panel-body">
-        <table class="table">
-          <thead>
-            <tr>
-              <th>Amount</th>
-              <th>Description</th>
-              <th>End date</th>
-            </tr>
-          </thead>
-	   <tbody>
-		
-	   </tbody>    	
-        </table>
+
 		<form>
-			<input type="text" amount="amount" size="18"> &emsp; 
-			<input type="text" description="description" size="20"> &emsp; &emsp; &nbsp
-			<input type="text" endDate="endDate" size="10"> &emsp; <br>
-			<input type="submit" value="Submit">
+			Amount:<br><input type="number" name="amount"><br>
+			Description:<br><input type="text" name="description"><br>
+			End date:<br><input type="date" name="end_date"><br>
+			<br><input type="submit" value="Submit">
+			<input type="reset">
 		</form>
       </div>
     </div>
--- website/transaction_form.php
+++ website/transaction_form.php
@@ -1,30 +1,18 @@
 <?php include "header.php";?>
 <html>
-<div class="col-xs-7" style="padding-left: 0">
+<div class="col-xl-12" style="padding-left: 0">
     <div class="panel panel-default">
       <div class="panel-heading">Enter Transaction</div>
       <div class="panel-body">
-        <table class="table">
-          <thead>
-            <tr>
-              <th>Transaction Amount</th>
-              <th>Account Balance</th>
-              <th>Category</th>
-	      <th>Description</th>
- 	      <th>Date</th>
-            </tr>
-          </thead>
-	   <tbody>
-		
-	   </tbody>    	
-        </table>
-<form>
-			<input type="text" transactionAmount="transactionAmount" size="20"> &emsp; &emsp; 
-			<input type="text" accountBalance="accountBalance" size="18"> &emsp; 
-			<input type="text" category="category" size="10"> &emsp; 
-			<input type="text" description="description" size="10"> &nbsp;
-			<input type="text" date="date" size="7"> &nbsp; <br>
-			<input type="submit" value="Submit">
+
+		<form>
+			Transaction Amount:<br><input type="number" name="amount"><br>
+			Description:<br><input type="text" name="description"><br>
+			Category:<br><input type="text" name="category"><br>
+			Date:<br><input type="date" name="date"><br>
+			Account Balance:<br><input type="number" name="balance"><br>
+			<br><input type="submit" value="Submit">
+			<input type="reset">
 		</form>
       </div>
     </div>
