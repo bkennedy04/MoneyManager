859fe73d025eae032b3ec6b571c21f25dca6dfe2
dkelly02 <dkelly02@bellarmine.edu>
2017-04-02 20:18:41 -0400

    ch-ch-ch-changes

--- website/goals_form.php
+++ website/goals_form.php
@@ -1,4 +1,27 @@
 <?php include "header.php";?>
-
-
-hello goals
+<html>
+<div class="col-xs-6" style="padding-left: 0">
+    <div class="panel panel-default">
+      <div class="panel-heading">Enter Goal</div>
+      <div class="panel-body">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Amount</th>
+              <th>Description</th>
+              <th>End date</th>
+            </tr>
+          </thead>
+	   <tbody>
+		
+	   </tbody>    	
+        </table>
+		<form>
+			<input type="text" amount="amount" size="18"> &emsp; 
+			<input type="text" description="description" size="20"> &emsp; &emsp; &nbsp
+			<input type="text" endDate="endDate" size="10"> &emsp; <br>
+			<input type="submit" value="Submit">
+		</form>
+      </div>
+    </div>
+</html>	
--- website/transaction_form.php
+++ website/transaction_form.php
@@ -1,4 +1,31 @@
 <?php include "header.php";?>
-
-
-hello transactions
+<html>
+<div class="col-xs-7" style="padding-left: 0">
+    <div class="panel panel-default">
+      <div class="panel-heading">Enter Transaction</div>
+      <div class="panel-body">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Transaction Amount</th>
+              <th>Account Balance</th>
+              <th>Category</th>
+	      <th>Description</th>
+ 	      <th>Date</th>
+            </tr>
+          </thead>
+	   <tbody>
+		
+	   </tbody>    	
+        </table>
+<form>
+			<input type="text" transactionAmount="transactionAmount" size="20"> &emsp; &emsp; 
+			<input type="text" accountBalance="accountBalance" size="18"> &emsp; 
+			<input type="text" category="category" size="10"> &emsp; 
+			<input type="text" description="description" size="10"> &nbsp;
+			<input type="text" date="date" size="7"> &nbsp; <br>
+			<input type="submit" value="Submit">
+		</form>
+      </div>
+    </div>
+</html>	
